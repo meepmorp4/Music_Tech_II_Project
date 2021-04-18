77c6901d39b84d94199671e4e64d81f79e819df5
Michael Ye <53098627+meepmorp4@users.noreply.github.com>
2021-04-18 17:09:27 -0400

    Add files via upload
    Cleaned up code a bit; created functions to get gamestate and inputs so that the main while loop doesn't get cluttetered.

--- main_code.py
+++ main_code.py
@@ -23,22 +23,67 @@ client = OSCClient()
 ip = "127.0.0.1"    # Localhost
 port = 6449
 client.connect( (ip, port) )
-player = "/player"  # player address
-enemy = "/enemy"    # opponent address
-# Character info ordered as:
+
+# OSC addresses
+playerAdd = "/playerState"          # for gamestate info
+enemyAdd = "/enemyState"            # for gamestate info
+playerInputAdd = "/playerInput"     # for actual inputs
+enemyInputAdd = "/enemyInput"       # for actual inputs
+
+# Character gamestate info ordered as:
 # x, y, on_ground, off_stage, on_platform
-playerinfo = [0, 0, 0, 0, 0]
-enemyinfo = [0, 0, 0, 0, 0]
-# This will make more sense later, when a message is sent.
+playerState = [0, 0, 0, 0, 0]
+enemyState = [0, 0, 0, 0, 0]
+# Get gamestate 
+def get_gamestate(states_list, port):
+    states_list[0] = int(gamestate.player[port].x)
+    states_list[1] = int(gamestate.player[port].y)
+    states_list[2] = int(gamestate.player[port].on_ground) # boolean
+    states_list[3] = int(gamestate.player[port].off_stage) # boolean
+    # I'm defining "on_platform" as having a Y position > 1 
+    #  while still being considered "on_ground".
+    if (states_list[1] > 1 and states_list[2] == 1): 
+        states_list[4] = 1
+    else:
+        states_list[4] = 0
+    # (int(gamestate.player[2].percent))
+    # (int(gamestate.player[2].stock))
+
+# Button inputs ordered as:
+# A, B, X, Y, Z, L, R, D_UP, D_DOWN, D_LEFT, D_RIGHT, (11 booleans)
+# main_stick_x, main_stick_y, c_sticK_x, c_stick_y, l_shoulder, r_shoulder (6 floats)
+playerInput = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+enemyInput = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+def get_inputs(input_list, port):
+    input_list[0] = int(gamestate.player[port].controller_state.button[Button.BUTTON_A])
+    input_list[1] = int(gamestate.player[port].controller_state.button[Button.BUTTON_B])
+    input_list[2] = int(gamestate.player[port].controller_state.button[Button.BUTTON_X])
+    input_list[3] = int(gamestate.player[port].controller_state.button[Button.BUTTON_Y])
+    input_list[4] = int(gamestate.player[port].controller_state.button[Button.BUTTON_Z])
+    input_list[5] = int(gamestate.player[port].controller_state.button[Button.BUTTON_L])
+    input_list[6] = int(gamestate.player[port].controller_state.button[Button.BUTTON_R])
+    input_list[7] = int(gamestate.player[port].controller_state.button[Button.BUTTON_D_UP])
+    input_list[8] = int(gamestate.player[port].controller_state.button[Button.BUTTON_D_DOWN])
+    input_list[9] = int(gamestate.player[port].controller_state.button[Button.BUTTON_D_LEFT])
+    input_list[10] = int(gamestate.player[port].controller_state.button[Button.BUTTON_D_RIGHT])
+    input_list[11] = (gamestate.player[port].controller_state.main_stick[0]) # main stick x value (0 to 1)
+    input_list[12] = (gamestate.player[port].controller_state.main_stick[1]) # main stick y value (0 to 1)
+    input_list[13] = (gamestate.player[port].controller_state.c_stick[0]) # c stick x value (0 to 1)
+    input_list[14] = (gamestate.player[port].controller_state.c_stick[1]) # c stick y value (0 to 1)
+    input_list[15] = (gamestate.player[port].controller_state.l_shoulder) # l shoulder bumper (0 to 1)
+    input_list[16] = (gamestate.player[port].controller_state.r_shoulder) # r shoulder bumper (0 to 1)
+
+# Port assignment
+playerPort = 2
+enemyPort = 1
 
 # Creates a console object that represents Slippi Online / Dolphin instance.
 console = melee.Console(path="C:/Users/Michael/Documents/_Spring 2021/Music and Tech 2/FM-Slippi-2.2.5-Win")
 # Note: File path must use forward slashes or double back slashes.
 #       Single back slashes will result in unicode error.
 # Creates controller objects for the bot (port 1) and the human (port 2).
-controller_bot = melee.Controller(console=console, port=1)
-controller_human = melee.Controller(console=console,
-                                    port=2,
+controller_bot = melee.Controller(console=console, port=enemyPort)
+controller_human = melee.Controller(console=console, port=playerPort,
                                     type=melee.ControllerType.GCN_ADAPTER)
 # Runs the Dolphin instance and connects the program code to it.
 console.run()
@@ -60,32 +105,14 @@ while True:
         #####################################
         # Sending OSC messages to Pure Data #
         #####################################        
-        playerinfo[0] = int(gamestate.player[2].x)
-        playerinfo[1] = int(gamestate.player[2].y)
-        playerinfo[2] = int(gamestate.player[2].on_ground) # boolean
-        playerinfo[3] = int(gamestate.player[2].off_stage) # boolean
-        # I'm defining "on_platform" as having a Y position > 1 
-        #  while still being considered "on_ground".
-        if (playerinfo[1] > 1 and playerinfo[2] == 1): 
-            playerinfo[4] = 1
-        else:
-            playerinfo[4] = 0
-        # msg.add_arg(int(gamestate.player[2].percent))
-        # msg.add_arg(int(gamestate.player[2].stock))
+        get_gamestate(playerState, playerPort)
+        get_gamestate(enemyState, enemyPort)
 
-        enemyinfo[0] = int(gamestate.player[1].x)
-        enemyinfo[1] = int(gamestate.player[1].y)
-        enemyinfo[2] = int(gamestate.player[1].on_ground) # boolean
-        enemyinfo[3] = int(gamestate.player[1].off_stage) # boolean
-        # I'm defining "on_platform" as having a Y position > 1 
-        #  while still being considered "on_ground".
-        if (enemyinfo[1] > 1 and enemyinfo[2] == 1): 
-            enemyinfo[4] = 1
-        else:
-            enemyinfo[4] = 0
-
-        client.send( OSCMessage(player, playerinfo))
-        client.send( OSCMessage(enemy, enemyinfo))
+        client.send( OSCMessage(playerAdd, playerState))
+        client.send( OSCMessage(enemyAdd, enemyState))
+        client.send( OSCMessage(playerInputAdd, playerInput))
+        client.send( OSCMessage(enemyInputAdd, enemyInput))
+            
 
     # If we're not in a match (e.g. menu, score screen)
     else:
@@ -99,3 +126,5 @@ while True:
             costume=1,      # The bot's color palette.
             autostart=False,# Whether player must press start to begin match.
             swag=False) # Whether bot annoys you while you choose your character.
+
+
